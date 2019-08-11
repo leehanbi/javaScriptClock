e6ba25a020a9a4ad0e5d9c28ccf7deb23f3e3950
leehanbi <dlgksql05@gmail.com>
2019-08-11 13:39:48 +0900

    file name 수정 todo.js코드 추가

--- todo.js
+++ todo.js
@@ -1,16 +1,25 @@
 const toDoForm = document.querySelector(".js-toDoForm"),
     todoInput = toDoForm.querySelector("input"),
-    todoList = document.querySelector("js-toDoList");
+    todoList = document.querySelector(".js-toDoList");
 
 const TODOS_LS = 'toDos';
 
 function paintToDo(text){
-    console.log(text);
+    const li = document.createElement("li");
+    const deleteBtn = document.createElement("button");
+    deleteBtn.innerHTML = "❌";
+    const span = document.createElement("span");
+    span.innerText = text;
+    li.appendChild(deleteBtn);
+    li.appendChild(span);
+    todoList.appendChild(li);
+
 }
 function handleSubmit(event){
     event.preventDefault();
     const currentValue = todoInput.value;
     paintToDo(currentValue);
+    todoInput.value = "";
 }
 function loadToDos(){
     const toDos = localStorage.getItem(TODOS_LS);
@@ -23,7 +32,6 @@ function toDoListInit(){
     loadToDos();
     toDoForm.addEventListener("submit",handleSubmit)
     
-
 }
 
 toDoListInit();
