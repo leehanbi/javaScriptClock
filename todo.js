7f0ab9a9afcf40b663b000fa1c6fb072768edb6d
leehanbi <dlgksql05@gmail.com>
2019-08-12 12:54:04 +0900

    코드 수정 localStrorage저장 추가

--- todo.js
+++ todo.js
@@ -3,17 +3,28 @@ const toDoForm = document.querySelector(".js-toDoForm"),
     todoList = document.querySelector(".js-toDoList");
 
 const TODOS_LS = 'toDos';
-
+const toDos = [];
+function saveToDos(){
+    //JSON.stringify()는 Object의 형태를 String의 형태로 바꿔준다. 
+    localStorage.setItem(TODOS_LS,JSON.stringify(toDos));
+}
 function paintToDo(text){
     const li = document.createElement("li");
     const deleteBtn = document.createElement("button");
-    deleteBtn.innerHTML = "❌";
     const span = document.createElement("span");
+    const newId = toDos.length + 1;
+    deleteBtn.innerHTML = "❌";
     span.innerText = text;
     li.appendChild(deleteBtn);
     li.appendChild(span);
+    li.id = newId;
     todoList.appendChild(li);
-
+    const toDoObj ={
+        text: text,
+        id: newId
+    }
+    toDos.push(toDoObj);
+    saveToDos();
 }
 function handleSubmit(event){
     event.preventDefault();
@@ -22,9 +33,12 @@ function handleSubmit(event){
     todoInput.value = "";
 }
 function loadToDos(){
-    const toDos = localStorage.getItem(TODOS_LS);
-    if(toDos !== null){
-
+    const loadedToDos = localStorage.getItem(TODOS_LS);
+    if(loadedToDos !== null){
+        const parsedToDos = JSON.parse(loadedToDos);
+        parsedToDos.forEach(function(toDo){
+            console.log(toDo.text);
+        });
     }
 }
 
