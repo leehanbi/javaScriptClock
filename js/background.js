fd63513907c2f5f3dc4220901c80a4a10b9c78de
leehanbi <dlgksql05@gmail.com>
2019-08-14 15:33:08 +0900

    코드 라인 정리 툴 설치 날씨 API 추가

--- js/background.js
+++ js/background.js
@@ -1,28 +1,28 @@
-const body = document.querySelector("body");
-// 배경화면 랜덤 수 
-const IMG_NUMBER =9;
+const body = document.querySelector("body")
+// 배경화면 랜덤 수
+const IMG_NUMBER = 9
 
-// 배경화면 뽑기 
-function paintImage(imageNumber){
-    const image = new Image();
+// 배경화면 뽑기
+function paintImage(imageNumber) {
+  const image = new Image()
 
-    image.src= `images/${imageNumber + 1}.jpg`;
-    image.classList.add("bgImage");
-    body.prepend(image);
+  image.src = `images/${imageNumber + 1}.jpg`
+  image.classList.add("bgImage")
+  body.prepend(image)
 }
 
 // 배경화면 랜덤 넘버 뽑기
-function genRandomNumber(){
-    const number = Math.floor(Math.random()*IMG_NUMBER);;
-    return number;
+function genRandomNumber() {
+  const number = Math.floor(Math.random() * IMG_NUMBER)
+  return number
 }
 
-// 초기 작업 
-function init(){
-    // 랜덤 수 취득
-    const randomNumber = genRandomNumber();
-    // 배경화면 뽑기 
-    paintImage(randomNumber);
+// 초기 작업
+function init() {
+  // 랜덤 수 취득
+  const randomNumber = genRandomNumber()
+  // 배경화면 뽑기
+  paintImage(randomNumber)
 }
 
-init();
+init()
--- js/weather.js
+++ js/weather.js
@@ -1,10 +1,31 @@
 
-const COORDS = 'coords';
-const API_KEY = "";
+const weather = document.querySelector(".js-weather");
+const COORDS = "coords";
+// 날씨 취득 APIKEY
+const API_KEY = "d540de0c3d2dbd20d0fbcb6569e34643";
+// 날씨 취득하기 
+function getWeather(latitude,longitude){
+    // API 호출 패스 
+    fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
+        ).then(function(response){
+            // fetch작업이 끝날 때까지 기다림.
+            return response.json()
+        }).then(function(json){
+            // 데이터가 준비되면 데이터 내용 출력
+            // 온도 
+            const temp = json.main.temp;
+            // 로컬 위치 
+            const place = json.name;
+            // 화면에 날씨,위치 출력 
+            weather.innerHTML = `${temp} @ ${place}`
+        });
+}
+
 function saveCoords(coordsObj){
     localStorage.setItem(COORDS,JSON.stringify(coordsObj));
 }
-
+// 로컬위치 취득 
 function handleGeoSucces(position){
     const latitude = position.coords.latitude;
     const longitude = position.coords.longitude;
@@ -14,24 +35,33 @@ function handleGeoSucces(position){
         longitude
     };
     saveCoords(coordsObj);
+    getWeather(latitude,longitude);
 }
 
+// 로컬위치 취득 에러 
 function gandleGeoError(){
-    console.log('Can`t access geo localtion ');
+    console.log("Can`t access geo localtion");
 }
 
+// 로컬위치 취득하기 
 function askForCoords(){
+    // 취득 성공:handleGeoSucces , 취득실패 : gandleGeoError
     navigator.geolocation.getCurrentPosition(handleGeoSucces,gandleGeoError);
 }
 
 function loadCoords(){
-    const loadedCords = localStorage.getItem(COORDS);
-    if (loadedCords === null){
+    // 현재 로컬 위치 취득 
+    const loadedCoords = localStorage.getItem(COORDS);
+    // 로컬 위치가 비어있다면 로컬위치 취득 
+    if (loadedCoords === null){
         askForCoords();
     }else{
+        const parseCoords = JSON.parse(loadedCoords);
+        getWeather(parseCoords.latitude,parseCoords.longitude);
         //get Weather
     }
 }
+// 초기작업 
 function init(){
     loadCoords();
 };
